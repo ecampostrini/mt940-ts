ff5748602bc0dda56484b77463004a2398c31074
Esteban Campostrini <lesteban22@gmail.com>
2021-05-27 15:17:46 +0200

    Remove unnecessary enum from the tokenizer

--- src/tokenizer.ts
+++ src/tokenizer.ts
@@ -1,8 +1,4 @@
-enum TOKEN {
-  TAG = '[0-9]{2}[A-Z]?',
-}
-
-const TAG = RegExp(`^:(${TOKEN.TAG}):`);
+const TAG = /^:([0-9]{2}[A-Z]?):/;
 
 const BLOCK_START = /^{([1-9][0-9]{0,2}|[A-Z]{1,3}):/;
 
@@ -12,16 +8,16 @@ const MINUS = /^(-)/;
 
 const COLON = /^(:)/;
 
-const NON_TEXTUAL_CHARACTERS = RegExp("({|})");
+const NON_TEXTUAL_CHARACTERS = /({|})/;
 
 export type SYMBOL =
-  | 'TAG'
-  | 'BLOCK_START'
-  | 'BLOCK_END'
-  | 'MINUS'
-  | 'TEXT'
-  | 'COLON'
-  | 'EOF';
+  | "TAG"
+  | "BLOCK_START"
+  | "BLOCK_END"
+  | "MINUS"
+  | "TEXT"
+  | "COLON"
+  | "EOF";
 
 export interface Token {
   line: number;
@@ -35,7 +31,7 @@ export class Tokenizer {
   private generator: Generator<Token, Token>;
 
   constructor(content: string) {
-    this.lines = content.split('\n').map((line) => line.trim());
+    this.lines = content.split("\n").map((line) => line.trim());
     this.generator = this.tokenizer();
     this.nextToken = this.generator.next().value;
   }
@@ -61,14 +57,14 @@ export class Tokenizer {
         : this.lines[lineCount].slice(lineOffset);
 
       if ((match = TAG.exec(line))) {
-        yield { line: lineCount + 1, type: 'TAG', content: match[1] };
+        yield { line: lineCount + 1, type: "TAG", content: match[1] };
         lineOffset += match[0].length;
         // skip the line counter increment in order to consume the rest of the line
         continue;
       } else if ((match = BLOCK_START.exec(line))) {
         yield {
           line: lineCount + 1,
-          type: 'BLOCK_START',
+          type: "BLOCK_START",
           content: match[1],
         };
         lineOffset += match[0].length;
@@ -77,19 +73,19 @@ export class Tokenizer {
       } else if ((match = BLOCK_END.exec(line))) {
         yield {
           line: lineCount + 1,
-          type: 'BLOCK_END',
+          type: "BLOCK_END",
           content: match[1],
         };
         lineOffset += match[0].length;
         // skip the line counter increment in order to consume the rest of the line
         continue;
       } else if (MINUS.test(line)) {
-        yield {line: lineCount - 1, type: 'MINUS', content: '-'};
+        yield { line: lineCount - 1, type: "MINUS", content: "-" };
       } else if ((match = COLON.exec(line))) {
-        yield { line: lineCount + 1, type: 'COLON', content: match[1] };
+        yield { line: lineCount + 1, type: "COLON", content: match[1] };
         lineOffset += match[0].length;
         // skip the line counter increment in order to consume the rest of the line
-        continue
+        continue;
       } else if (!line) {
         // skip empty lines
       } else {
@@ -97,20 +93,20 @@ export class Tokenizer {
         if (match) {
           yield {
             line: lineCount + 1,
-            type: 'TEXT',
+            type: "TEXT",
             content: line.substring(0, match.index),
           };
           // skip the line counter increment in order to consume the rest of the line
           lineOffset += match.index;
           continue;
         }
-        yield { line: lineCount + 1, type: 'TEXT', content: line };
+        yield { line: lineCount + 1, type: "TEXT", content: line };
       }
 
       lineCount++;
       lineOffset = 0;
     }
 
-    return { line: lineCount, type: 'EOF', content: '' } as Token;
+    return { line: lineCount, type: "EOF", content: "" } as Token;
   }
 }
