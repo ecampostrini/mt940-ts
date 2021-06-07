353d35fcaf9e2a7aff6fa6fb17e025a01b359223
Esteban Campostrini <lesteban22@gmail.com>
2021-06-07 14:01:42 +0200

    fix(src): #1 Treat dash as TEXT unless is the only character in the line - Plus other minor adjustments to the tokeinzer:

--- src/tokenizer.ts
+++ src/tokenizer.ts
@@ -2,13 +2,9 @@ const TAG = /^:([0-9]{2}[A-Z]?):/;
 
 const BLOCK_START = /^{([1-9][0-9]{0,2}|[A-Z]{1,3}):/;
 
-const BLOCK_END = /^(})/;
+const BLOCK_END = /^(-?})/;
 
-const MINUS = /^(-)/;
-
-const COLON = /^(:)/;
-
-const NON_TEXTUAL_CHARACTERS = /({|})/;
+const MINUS = /^-$/;
 
 export type SYMBOL =
   | "TAG"
@@ -16,7 +12,6 @@ export type SYMBOL =
   | "BLOCK_END"
   | "MINUS"
   | "TEXT"
-  | "COLON"
   | "EOF";
 
 export interface Token {
@@ -31,7 +26,7 @@ export class Tokenizer {
   private generator: Generator<Token, Token>;
 
   constructor(content: string) {
-    this.lines = content.split("\n").map((line) => line.trim());
+    this.lines = content.split("\r\n");
     this.generator = this.tokenizer();
     this.nextToken = this.generator.next().value;
   }
@@ -80,26 +75,8 @@ export class Tokenizer {
         // skip the line counter increment in order to consume the rest of the line
         continue;
       } else if (MINUS.test(line)) {
-        yield { line: lineCount - 1, type: "MINUS", content: "-" };
-      } else if ((match = COLON.exec(line))) {
-        yield { line: lineCount + 1, type: "COLON", content: match[1] };
-        lineOffset += match[0].length;
-        // skip the line counter increment in order to consume the rest of the line
-        continue;
-      } else if (!line) {
-        // skip empty lines
+        yield { line: lineCount + 1, type: "MINUS", content: "-" };
       } else {
-        match = NON_TEXTUAL_CHARACTERS.exec(line);
-        if (match) {
-          yield {
-            line: lineCount + 1,
-            type: "TEXT",
-            content: line.substring(0, match.index),
-          };
-          // skip the line counter increment in order to consume the rest of the line
-          lineOffset += match.index;
-          continue;
-        }
         yield { line: lineCount + 1, type: "TEXT", content: line };
       }
 
